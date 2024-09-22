3a125b9566d47dcf4d267469d8a688096c15ac04
Will-Andrade <willian.santos.andra@outlook.com>
2024-09-22 14:31:03 -0400

    feat: added the Course and Mentorship domain classes

--- src/main/java/org/andradev/Main.java
+++ src/main/java/org/andradev/Main.java
@@ -1,7 +1,29 @@
 package org.andradev;
 
+import org.andradev.domain.Course;
+import org.andradev.domain.Mentorship;
+
+import java.time.LocalDate;
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		Course course1 = new Course();
+		course1.setTitle("Java course");
+		course1.setDescription("Basics of Java programming");
+		course1.setWorkload(8);
+		
+		Course course2 = new Course();
+		course2.setTitle("JavaScript course");
+		course2.setDescription("Basics of JavaScript programming");
+		course2.setWorkload(9);
+		
+		Mentorship mentorship = new Mentorship();
+		mentorship.setTitle("Java mentorship");
+		mentorship.setDescription("A mentorship on the intrinsics of Java code");
+		mentorship.setDate(LocalDate.now());
+		
+		System.out.println(course1);
+		System.out.println(course2);
+		System.out.println(mentorship);
 	}
 }
