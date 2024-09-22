96031dbe2d297fb39c1df769b47cf8dfc889fe52
Will-Andrade <willian.santos.andra@outlook.com>
2024-09-22 15:34:15 -0400

    feat: testing implementations

--- src/main/java/org/andradev/Main.java
+++ src/main/java/org/andradev/Main.java
@@ -1,7 +1,6 @@
 package org.andradev;
 
-import org.andradev.domain.Course;
-import org.andradev.domain.Mentorship;
+import org.andradev.domain.*;
 
 import java.time.LocalDate;
 
@@ -22,8 +21,36 @@ public class Main {
 		mentorship.setDescription("A mentorship on the intrinsics of Java code");
 		mentorship.setDate(LocalDate.now());
 		
-		System.out.println(course1);
-		System.out.println(course2);
-		System.out.println(mentorship);
+		Bootcamp bootcamp = new Bootcamp();
+		bootcamp.setName("Bootcamp Java Developer");
+		bootcamp.setDescription("Bootcamp Java Developer - From 0 to 1");
+		bootcamp.getContents().add(course1);
+		bootcamp.getContents().add(course2);
+		bootcamp.getContents().add(mentorship);
+		
+		Dev willDev = new Dev();
+		willDev.setName("Willian");
+		
+		willDev.subscribeBootcamp(bootcamp);
+		
+		willDev.progress();
+		willDev.progress();
+		
+		System.out.println("Conteúdos Inscritos Willian: " + willDev.getSubscribedContent());
+		System.out.println("Conteúdos Concluídos Willian: " + willDev.getFinishedContents());
+		System.out.println("XP: " + willDev.calculateTotalXP());
+		
+		Dev johnDev = new Dev();
+		johnDev.setName("John");
+		
+		johnDev.subscribeBootcamp(bootcamp);
+		
+		johnDev.progress();
+		johnDev.progress();
+		johnDev.progress();
+		
+		System.out.println("Conteúdos Inscritos John: " + johnDev.getSubscribedContent());
+		System.out.println("Conteúdos Concluídos John: " + johnDev.getFinishedContents());
+		System.out.println("XP: " + johnDev.calculateTotalXP());
 	}
 }
